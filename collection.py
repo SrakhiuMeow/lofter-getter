09636150892282e0eb198f9a6a6dfaa82d11b9ac
SrakhiuMeow <srakhiumeow@gmail.com>
2024-10-08 15:59:29 +0800

    增加parser

--- collection.py
+++ collection.py
@@ -1,10 +1,21 @@
 from utils.save_content import save_single_collection
+import argparse
 
 # 保存路径
 save_path = './results'
 
 # 合集ID，可根据APP端的分享链接获取
-collection_id = 21297998
+collection_id = 20194917
 
-# 保存合集
-save_single_collection(collection_id, save_path, save_img=False, rewrite=False)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Save single collection from lofter')
+    
+    parser.add_argument('collection_id', type=int, default=collection_id ,help='Collection ID')
+    parser.add_argument('--save_path', type=str, default=save_path, help='Save path')
+    parser.add_argument('--save_img', action='store_true', help='Save images')
+    parser.add_argument('--rewrite', action='store_true', help='Rewrite existing files')
+
+    args = parser.parse_args()
+
+    # 保存合集
+    save_single_collection(args.collection_id, args.save_path, save_img=args.save_img, rewrite=args.rewrite)
--- single_post.py
+++ single_post.py
@@ -1,4 +1,5 @@
 from utils.save_content import save_single_post
+import argparse
 
 
 # 保存路径
@@ -8,4 +9,15 @@ save_path = './results'
 blog_id = 0
 post_id = 0
 
-save_single_post(blog_id, post_id)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Save single post from lofter')
+    
+    parser.add_argument('blog_id', type=int, default=blog_id, help='Blog ID')
+    parser.add_argument('post_id', type=int, default=post_id, help='Post ID')
+    parser.add_argument('--save_path', type=str, default=save_path, help='Save path')
+    parser.add_argument('--rewrite', action='store_true', help='Rewrite existing files')
+
+    args = parser.parse_args()
+
+    # 保存文章
+    save_single_post(args.blog_id, args.post_id, args.save_path, rewrite=args.rewrite)
